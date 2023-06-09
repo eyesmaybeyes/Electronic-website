8864862cbdcc01fe1030832b90c02234615e9321
Maria <114153517+eyesmaybeyes@users.noreply.github.com>
2023-06-09 22:42:19 +0300

    added text around the circle

--- index.js
+++ index.js
@@ -37,3 +37,30 @@ listItems.forEach(item => {
         item.classList.add('active');
     });
 });
+// расстояние между словами
+const rotateBetweenWords = (value) => {
+    const words = document.querySelectorAll('.wheel__word')
+
+    let deg = 263;
+
+    for (let word of words) {
+        word.style.transform = `rotate(${deg}deg)`
+        deg += value
+    }
+}
+
+// расстояние между буквами
+const rotateBetweenLetters = (value) => {
+    const letters = document.querySelectorAll('.wheel__letter')
+
+    let deg = 0
+
+    for (let letter of letters) {
+        letter.style.transform = `rotate(${deg}deg)`
+        deg += value
+    }
+
+}
+
+rotateBetweenWords(3)
+rotateBetweenLetters(9)
